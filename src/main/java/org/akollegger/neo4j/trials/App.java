66d1849b1b208009b7472007ea6a39e793e88ea6
Andreas Kollegger <akollegger@gmail.com>
2011-12-29 10:02:18 -0500

    mvn archetype generated project

---
+++ src/main/java/org/akollegger/neo4j/trials/App.java
@@ -0,0 +1,13 @@
+package org.akollegger.neo4j.trials;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
